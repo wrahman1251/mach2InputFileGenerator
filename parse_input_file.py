fe9e77cf8da82bc114524b3d8060b45c0867a520
Walid Rahman <waleedda1@gmail.com>
2018-01-14 20:58:05 -0500

    Parsed all contents from original input file to JSON using parse_input_file.py

---
+++ parse_input_file.py
@@ -0,0 +1,49 @@
+import json
+
+# Read input file line by line
+# if variable, look for parameters
+# use both variable and parameters to build dictionary
+#	in the same format as our json template
+# convert dictionary to json format and write to file
+
+f = open('input_Kr.txt', 'r')
+lines = list(f)
+
+all_vars = []
+
+i = 0
+while i < len(lines):
+	if "$" in lines[i]: #variable begins
+		var_name = lines[i].strip(" $").rstrip("\n")
+		params = []
+
+		while lines[i].strip(" $") != "end\n":
+			if "=" in lines[i]:
+				param = {}
+				param_valdesc = lines[i].split("=")
+				val_desc = param_valdesc[1].split(",")
+				param["name"] = param_valdesc[0].strip()
+				param["value"] = val_desc[0].strip()
+
+				if len(val_desc) > 1:
+					param["description"] = val_desc[1].strip(" !").rstrip("\n")
+				else:
+					param["description"] = ""
+				params.append(param)
+
+			i += 1
+
+		variable = {"type": "var", "description": ""}
+		variable["name"] = var_name
+		variable["data"] = {"parameters": params}
+
+		all_vars.append(variable)
+
+	i+=1
+
+new_f = open('input_Kr.json', 'w+')
+
+new_f.write(json.dumps(all_vars))
+
+new_f.close()
+f.close()	
